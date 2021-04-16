39dbf3b3f8164d7c32b2b7a14930ca31b6c006cf
SeregaBess <seregabarahsev836@gmail.com>
2021-04-16 19:35:19 +0500

    test

---
+++ d
@@ -0,0 +1,19 @@
+function defaultTask(cb) {
+  // place code for your default task here
+  cb();
+}
+
+exports.default = defaultTask
+
+
+
+{
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"]
+    }
+}
+
+
+
+"extends": "airbnb-base",
---
+++ gulp/config.js
@@ -0,0 +1,31 @@
+const srcPath = 'src';
+const destPath = 'build';
+
+const config = {
+    src: {
+        root: srcPath,
+        sass: '${srcPath}/scss',
+        js: '${srcPath}/js',
+        fonts: '${srcPath}/assets/fonts',
+        image: '${srcPath}/assets/images',
+        iconMono: '${srcPath}/assets/mono',
+        iconMulti: '${srcPath}/assets/multi',
+        pug: '${srcPath}/pug',
+    },
+
+    dest: {
+        root: destPath,
+        html: destPath,
+        css:'${srcPath}/css',
+        js:'${srcPath}/js',
+        fonts:'${srcPath}/fonts',
+        images:'${srcPath}/images',
+    },
+
+    setEnv() {
+      this.isProd = process.argv.includes('--prod');
+      this.isDev = !this.isProd;
+    }
+};
+
+export default config;
