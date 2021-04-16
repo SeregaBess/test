6cfba34c1e2fa3645d17bfdfac26635172cdedd7
SeregaBess <seregabarahsev836@gmail.com>
2021-04-16 20:27:11 +0500

    test

--- gulp/config.js
+++ gulp/config.js
@@ -2,30 +2,30 @@ const srcPath = 'src';
 const destPath = 'build';
 
 const config = {
-    src: {
-        root: srcPath,
-        sass: '${srcPath}/scss',
-        js: '${srcPath}/js',
-        fonts: '${srcPath}/assets/fonts',
-        image: '${srcPath}/assets/images',
-        iconMono: '${srcPath}/assets/mono',
-        iconMulti: '${srcPath}/assets/multi',
-        pug: '${srcPath}/pug',
-    },
+  src: {
+    root: srcPath,
+    sass: `${srcPath}/scss`,
+    js: `${srcPath}/js`,
+    fonts: `${srcPath}/assets/fonts`,
+    image: `${srcPath}/assets/images`,
+    iconMono: `${srcPath}/assets/mono`,
+    iconMulti: `${srcPath}/assets/multi`,
+    pug: `${srcPath}/pug`,
+  },
 
-    dest: {
-        root: destPath,
-        html: destPath,
-        css:'${srcPath}/css',
-        js:'${srcPath}/js',
-        fonts:'${srcPath}/fonts',
-        images:'${srcPath}/images',
-    },
+  dest: {
+    root: destPath,
+    html: destPath,
+    css: `${srcPath}/css`,
+    js: `${srcPath}/js`,
+    fonts: `${srcPath}/fonts`,
+    images: `${srcPath}/images`,
+  },
 
-    setEnv() {
-      this.isProd = process.argv.includes('--prod');
-      this.isDev = !this.isProd;
-    }
+  setEnv() {
+    this.isProd = process.argv.includes('--prod');
+    this.isDev = !this.isProd;
+  },
 };
 
 export default config;
